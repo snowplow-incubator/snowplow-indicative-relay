fc7bcfdae2e5ad0e6db994b4608cfb7dd9e50c17
Scala Steward <me@scala-steward.org>
2019-08-05 02:09:42 +0200

    Update specs2-core, specs2-scalacheck to 4.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
     val circe            = "0.11.1"
     val catsEffect       = "1.3.1"
     val scalaj           = "2.4.2"
-    val specs2           = "4.5.1"
+    val specs2           = "4.7.0"
     val scalacheckSchema = "0.1.0"
   }
 
