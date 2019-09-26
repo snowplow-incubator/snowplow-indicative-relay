92d876f32a68b783932fbb7fa5964d800ef167f6
Scala Steward <me@scala-steward.org>
2019-09-26 23:24:53 +0200

    Update snowplow-scala-analytics-sdk to 0.4.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     val awsLambdaCore    = "1.2.0"
     val awsLambdaEvents  = "2.2.7"
     val kinesisClient    = "1.11.2"
-    val analyticsSdk     = "0.3.2"
+    val analyticsSdk     = "0.4.2"
     val circe            = "0.11.1"
     val catsEffect       = "1.4.0"
     val scalaj           = "2.4.2"
